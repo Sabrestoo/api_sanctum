91e5885d69b99ef8bf35d5ec2c1f6678457df2f5
Stuart Summers <summers2483@hotmail.com>
2021-04-01 21:30:50 +0200

    Sanctum AuthController, protected routes and token generation for protected routes

---
+++ database/seeders/testSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Database\Factories\ProductFactory;
+use Faker\Core\Number;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Type\Integer;
+
+
+class testSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::factory()
+            ->count(100)
+            ->create();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,12 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Public routes
 Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{product}', [ProductController::class, 'show']);
-Route::put('products/{product}', [ProductController::class, 'update']);
-Route::delete('products/{product}', [ProductController::class, 'destroy']);
+Route::get('/products/{product}', [ProductController::class, 'show']);;
 Route::get('products/search/{name}', [ProductController::class, 'search']);
+Route::post('/register', [AuthController::class, 'register']);
+
+//Auth routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('products/{product}', [ProductController::class, 'update']);
+    Route::delete('products/{product}', [ProductController::class, 'destroy']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
